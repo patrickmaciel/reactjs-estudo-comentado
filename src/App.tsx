53b7c9fb571697108b619450920950d326ae5eb9
Patrick Maciel <patrickmaciel.info@gmail.com>
2021-08-30 10:44:24 -0300

    clean default create-app react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// da forma abaixo, ele obriga a pessoa importar como App e não como qualquer nome, exemplo
+// import App from './App' e não import Batata from './App' (que funciona caso tenha o export default)
+export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
     </div>
   );
 }
 
-export default App;
+// quem define o nome do componente não é quem esta exportando, e sim quem esta importando
+// export default App;
